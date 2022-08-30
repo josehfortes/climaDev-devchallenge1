0a1bdb2e9d4d3fbe62e07b8bdd80b72e7647f2c1
josehfortes <vasco0007@gmail.com>
2022-08-30 20:34:39 -0300

    Melhorado index

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,11 +3,11 @@ import styled from 'styled-components'
 import BackgroundImageWithChildren from '../src/components/layout/backgroundImageWithChildren/BackgroundImageWithChildren'
 
 const StyledDiv = styled.div`
-  background-color: red;
+  background-color: ${props => props.theme.colors.white};
+  min-width: 500px;
 
-  * {
-    margin: 0;
-    padding: 0;
+  @media (max-width: 500px) {
+    min-width: 100%;
   }
 `
 
