88ef03839e8edeeff0e8cf40f6476c119b145179
josehfortes <vasco0007@gmail.com>
2022-09-06 20:11:14 -0300

    Criado logo

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,6 +6,11 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     margin: 0;
     background-color: ${props => props.theme.colors.white};
+    font-family: sans-serif;
+  }
+
+  * {
+    box-sizing: border-box;
   }
 `
 
