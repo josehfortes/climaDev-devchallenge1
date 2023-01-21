e65f23ed5b3e934ef2918c105ae96e87e433971e
Jose Fortes <vasco0007@gmail.com>
2023-01-21 12:48:08 -0300

    criado decodeURI no handle

--- pages/api/openweathermap/index.js
+++ pages/api/openweathermap/index.js
@@ -1,6 +1,6 @@
 export default async function handle(req, res) {
   console.log(req.url)
-  const url = req.url.replace('/api/openweathermap?url=', '')
+  const url = decodeURIComponent(req.url).replace('/api/openweathermap?url=', '')
   console.log(`https://api.openweathermap.org/${url}&appid=${process.env.OWM_KEY}`)
   const response = await fetch(`https://api.openweathermap.org/${url}&appid=${process.env.OWM_KEY}`)
   const json = await response.json()
