84b7c7305374e474cd80e1344ee5b3d077eaba34
Jose Fortes <vasco0007@gmail.com>
2023-01-21 12:40:19 -0300

    adicionado console log no index

--- pages/api/openweathermap/index.js
+++ pages/api/openweathermap/index.js
@@ -1,5 +1,7 @@
 export default async function handle(req, res) {
+  console.log(req.url)
   const url = req.url.replace('/api/openweathermap?url=', '')
+  console.log(`https://api.openweathermap.org/${url}&appid=${process.env.OWM_KEY}`)
   const response = await fetch(`https://api.openweathermap.org/${url}&appid=${process.env.OWM_KEY}`)
   const json = await response.json()
   res.status(response.status).json(json)
