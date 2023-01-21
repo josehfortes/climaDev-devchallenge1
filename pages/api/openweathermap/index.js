dc19c0f7d47e9ce921d29f43882a4eac7e4add57
Jose Fortes <vasco0007@gmail.com>
2023-01-21 12:52:18 -0300

    removido console log

--- pages/api/openweathermap/index.js
+++ pages/api/openweathermap/index.js
@@ -1,7 +1,5 @@
 export default async function handle(req, res) {
-  console.log(req.url)
   const url = decodeURIComponent(req.url).replace('/api/openweathermap?url=', '')
-  console.log(`https://api.openweathermap.org/${url}&appid=${process.env.OWM_KEY}`)
   const response = await fetch(`https://api.openweathermap.org/${url}&appid=${process.env.OWM_KEY}`)
   const json = await response.json()
   res.status(response.status).json(json)
